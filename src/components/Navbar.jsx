246231efae307a862467d56681e7cc227d17a3ef
jennyguidetti <j_guidetti95@hotmail.com>
2025-02-26 15:04:37 +1000

    fixing formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,23 @@
- 
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <Link to="/">
+                    <img src="/img/logo.png" alt="Logo" />
+                </Link>
+            </div>
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/projectsall">Services</Link>
+                <Link to="/signup">Sign Up</Link>
+                <Link to="/login">Login</Link>
+            </div>
+            <div className="red-line"></div>
+        </nav>
+    );
+}
+
+export default Navbar; 
